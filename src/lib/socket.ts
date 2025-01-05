99b975266b14bf44564295b5d3e9175e5881b3d3
Ercode Techoglu <ercode@pm.me>
2025-01-05 08:46:06 +0300

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,7 +5,7 @@ const SERVER_URL = "ws://35.158.220.135:3000";
 const socket = io(SERVER_URL, {
 	autoConnect: true,
 	reconnection: true,
-	transports: ["websocket"]
+	transports: ["websocket", "polling"]
 });
 
 export default socket;
